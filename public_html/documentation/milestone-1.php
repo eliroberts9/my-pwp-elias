f96334255df952e535a91e1f4865fd2eb12068e2
Elias Roberts <eliroberts9@gmail.com>
2018-07-14 12:41:50 -0600

    added purpose, audience, and goal sections to milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,9 +2,23 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Title</title>
+		<title>Elias Roberts PWP Milestone 1</title>
 	</head>
 	<body>
+		<h1>PWP - Milestone One</h1>
+
+		<h3>Purpose:</h3>
+
+		<p>My PWP will serve as a portfolio showcasing the skills I've developed throughout the bootcamp
+		as well as projects I've worked on.</p>
+		<br />
+
+		<h3>Audience:</h3>
+		<p>My audience will include potential employers, other developers and potentially clients for freelance work.</p>
+		<br />
+
+		<h3>Goal:</h3>
+		<p>The goal for my PWP is to acquire full-time employment as well as serve as a gallery to showcase my work.</p>
 		
 	</body>
 </html>
