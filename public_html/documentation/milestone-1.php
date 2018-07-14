08ce85689f28d5116afa2d985dd073e1b7458cb7
Elias Roberts <eliroberts9@gmail.com>
2018-07-14 13:02:14 -0600

    added Persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -19,6 +19,21 @@
 
 		<h3>Goal:</h3>
 		<p>The goal for my PWP is to acquire full-time employment as well as serve as a gallery to showcase my work.</p>
+		<br />
+
+		<h3>Persona:</h3>
+		<ul>
+			<li>Charles Calderon</li>
+			<li>Age: 42</li>
+			<li>Senior Developer</li>
+			<li>Charles takes pride in his work and expects the same of those he works with.  He will put every bit of
+			effort into ensuring his finished projects exceed expectations.</li>
+			<li>Charles has a higher than average understanding of all things tech.  His main device is a latest generation
+			Apple Macbook Pro, 15", 512GB storage, 2.6GHz 6-core 8th-generation i7 w/ 16 GB DDR4.  His mobile device is an
+			iPhone X w/ 256GB storage, a 5.8" screen w/ 2436 x 1125 resolution.</li>
+			<li>Charles' goal when visiting this site will be to better understand a potential employee's skill set and to
+			see examples of previous work.</li>
+		</ul>
 		
 	</body>
 </html>
